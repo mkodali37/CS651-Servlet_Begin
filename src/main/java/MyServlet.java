16aa45386a51fcf25e71dc5a3745a12865069747
mkodali37 <mkodali@horiozon.csueastbay.edu>
2021-03-18 14:10:06 -0700

    CS651 _Exercise_ServletBegin--commit to main

---
+++ src/main/java/MyServlet.java
@@ -0,0 +1,46 @@
+
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet({ "/MyServlet", "/Hello", "/" })
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private String message = "";
+
+    /**
+     * Default constructor. 
+     */
+    public MyServlet() {
+        // TODO Auto-generated constructor stub
+    	message = "Hello World!";
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		
+		out.println(message);
+		//response.getWriter().append("Hello World ");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
